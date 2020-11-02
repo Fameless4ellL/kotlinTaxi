1047de389b9de697e1665b4eec0c994d6c18a9b6
Zaur <fameless.4ell@gmail.com>
2020-11-02 16:02:05 +0100

    Information Panel

--- app/src/main/java/com/fameless/kotlintaxi/Common.kt
+++ app/src/main/java/com/fameless/kotlintaxi/Common.kt
@@ -1,8 +1,16 @@
 package com.fameless.kotlintaxi
 
 import com.fameless.kotlintaxi.Model.DriverInfoModel
+import java.lang.StringBuilder
 
 object Common {
+    fun buildWelcomeMessage(): String {
+        return StringBuilder("Welcome, ")
+            .append(currentUser!!.firstName)
+            .append(" ")
+            .append(currentUser!!.lastName)
+            .toString()
+    }
 
     val DRIVERS_LOCATION_REFERENCE: String = "DriversLocation"
     var currentUser: DriverInfoModel? = null
